6926fc81dedc4857f855102d23ea01725039d207
Marques Avery <averymarques4@gmail.com>
2020-09-10 21:48:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,8 @@ class Student
 
   def self.create_from_collection(students_array)
     binding.pry
+    students_array.each do |student|
+      self.new(student)
   end
 
   def add_student_attributes(attributes_hash)
