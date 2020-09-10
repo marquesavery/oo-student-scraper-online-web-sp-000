729c17776913e3e338224f24e390eea1d518c078
Marques Avery <averymarques4@gmail.com>
2020-09-10 21:24:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,8 +25,11 @@ class Scraper
     html = Nokogiri::HTML(open(profile_url))
     social_links = html.css(".social-icon-container").css("a/@href")
     social_links.each do |link|
-      if social_links[0].text.include?("twitter")
+      if link.text.include?("twitter")
         student_profile_hash[:twitter] = link.text
+      elsif link.text.include?("twitter")
+        student_profile_hash[:twitter] = link.text
+
 
     social_media = html.css(".social-icon-container")
 
