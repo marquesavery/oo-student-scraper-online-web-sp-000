0f235b86af2882e1309730916af6e4e44723cf34
Marques Avery <averymarques4@gmail.com>
2020-09-10 20:42:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,10 @@ class Scraper
 
 
   def self.scrape_index_page(index_url)
-    # binding.pry
     student_list = []
     html = Nokogiri::HTML(open(index_url))
     students = html.css(".student-card")
     students.each do |student|
-      # binding.pry
       student_hash = {}
       student_hash[:name] = student.css(".student-name").text
       student_hash[:location] = student.css(".student-location").text
