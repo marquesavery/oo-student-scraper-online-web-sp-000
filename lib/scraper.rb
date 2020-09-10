06919c9063b151fd3c5ab82db1b9e8fa44affd03
Marques Avery <averymarques4@gmail.com>
2020-09-10 19:57:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,12 @@ class Scraper
 
 
   def self.scrape_index_page(index_url)
-    binding.pry
+    
     student_list = []
     html = Nokogiri::HTML(open(index_url))
+
+    page.css(".student-name").text
+    page.css(".student-location").text
   end
 
   def self.scrape_profile_page(profile_url)
